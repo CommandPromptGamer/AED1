9ea8a99ec2db3438e7db83eafcef2340e4af3320
CommandPromptGamer <46406111+CommandPromptGamer@users.noreply.github.com>
2023-02-24 16:55:52 -0300

    Replace spaces with tabs to conform to convention

--- Staircase/Staircase.c
+++ Staircase/Staircase.c
@@ -3,11 +3,11 @@
 int Staircase_r( int step, int goal );
 
 int main() {
-    for ( int i = 1; i < 10; i++ ) { // Testa o Staircase_r() com uma quantidade de 1 a 10 degraus.
-        printf( "%d\n", Staircase_r( 0, i ) );
-    }
+	for ( int i = 1; i <= 10; i++ ) { // Testa o Staircase_r() com uma quantidade de 1 a 10 degraus.
+		printf( "%d\n", Staircase_r( 0, i ) );
+	}
 
-    return 0;
+	return 0;
 }
 
 int Staircase_r( int step, int goal ) {
@@ -22,19 +22,19 @@ Staircase_r
  bruta.
 ====================
 */
-    
-    int ammount = 0;
-    
-    if ( step + 1 > goal ) {
-        return 0;
-    } else if ( step + 1 == goal ) {
-        return 1;
-    } else if ( step + 2 == goal ) {
-        return 2;
-    } else {
-        ammount += Staircase_r( step + 1, goal );
-        ammount += Staircase_r( step + 2, goal );
-    }
 
-    return ammount;
+	int ammount = 0;
+
+	if ( step + 1 > goal ) {
+		return 0;
+	} else if ( step + 1 == goal ) {
+		return 1;
+	} else if ( step + 2 == goal ) {
+		return 2;
+	} else {
+		ammount += Staircase_r( step + 1, goal );
+		ammount += Staircase_r( step + 2, goal );
+	}
+
+	return ammount;
 }
