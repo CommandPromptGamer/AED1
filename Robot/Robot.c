1e8e17d0007e796d90c552d5d600ed9ada890b24
CommandPromptGamer <46406111+CommandPromptGamer@users.noreply.github.com>
2023-03-03 16:51:21 -0300

    Replace typedef'ed bool with <stdbool.h> type The bool type was defined with a typedef and no true and false #defines were made. Now the proper <stdbool.h> type is used along with true and false literals being written properly rather than 0 and 1. This will make migration to C2x easier, as C2x has a proper bool type built into the language and all that will be needed is to remove the <stdbool.h> include. The indentation has been replaced with 4 spaces tabs rather than spaces as required by style coonventions. A typo in a judgeCircle() function comment has been fixed: convension -> convention.

--- Robot/Robot.c
+++ Robot/Robot.c
@@ -1,47 +1,46 @@
 #include <stdio.h>
-
-typedef int bool; // Acceptable values: 0 and 1.
+#include <stdbool.h>
 
 bool judgeCircle(char * moves); // Does not conform to code style convention: carbon copy of assignment text.
 
 int main() {
-    judgeCircle( "UD" ) ? printf( "UD: true.\n" ) : printf( "UD: false.\n" ); // Tests.
-    judgeCircle( "LL" ) ? printf( "LL: true.\n" ) : printf( "LL: false.\n" );
+	judgeCircle( "UD" ) ? printf( "UD: true.\n" ) : printf( "UD: false.\n" ); // Tests.
+	judgeCircle( "LL" ) ? printf( "LL: true.\n" ) : printf( "LL: false.\n" );
 
-    return 0;
+	return 0;
 }
 
 bool judgeCircle(char * moves) {
-    int     xPos = 0;
-    int     yPos = 0;
-    char *  move = moves;
+	int     xPos = 0;
+	int     yPos = 0;
+	char *  move = moves;
 
-    while ( *move != '\0' ) {
-        switch ( *move ) {
-            case 'R':
-                xPos++;
-                break;
-            case 'L':
-                xPos--;
-                break;
-            case 'U':
-                yPos++;
-                break;
-            case 'D':
-                yPos--;
-                break;
-            
-            default:
-                return 0; // Invalid sequence returns false.
-                break;
-        }
+	while ( *move != '\0' ) {
+		switch ( *move ) {
+			case 'R':
+				xPos++;
+				break;
+			case 'L':
+				xPos--;
+				break;
+			case 'U':
+				yPos++;
+				break;
+			case 'D':
+				yPos--;
+				break;
+			
+			default:
+				return false; // Invalid sequence returns false.
+				break;
+		}
 
-        move++;
-    }
+		move++;
+	}
 
-    if ( xPos != 0 || yPos != 0 ) {
-        return 0;
-    } else {
-        return 1;
-    }
+	if ( xPos != 0 || yPos != 0 ) {
+		return false;
+	} else {
+		return true;
+	}
 }
