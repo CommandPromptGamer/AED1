a12fd2f2fc98749dfa6efc0c3c81543a79b28532
CommandPromptGamer <46406111+CommandPromptGamer@users.noreply.github.com>
2023-03-23 21:54:47 -0300

    Fix rebuild database

--- NoVariableContactBook/NoVariableContactBook.c
+++ NoVariableContactBook/NoVariableContactBook.c
@@ -7,7 +7,7 @@
 #define NODE_SIZE ( sizeof( void* ) * 3 )
 
 void ClearDatabase( void ** database );
-void RebuidDatabase( void ** database, void * pBuffer );
+void RebuildDatabase( void ** database, void * pBuffer );
 
 int main() {
 	void *  pBuffer = malloc( sizeof( int ) );
@@ -16,6 +16,7 @@ int main() {
 	*( int* )pBuffer = 0;
 
 	while (1) {
+		menu:
 		printf("Escolha uma opção: \n1 - Incluir registro\n2 - Apagar registro por nome\n3 - Buscar por um registro\n4 - Listar o arquivo\n5 - Sair\nEntre a sua opção: ");
 		
 		switch ( getchar() ) {
@@ -35,9 +36,9 @@ int main() {
 				scanf( "%lld", ( unsigned long long * )( ( char* )pBuffer + ( PBUFFER_OFFSET + ( PERSON_SIZE * ( *( int* )pBuffer ) ) + 10 * sizeof( char ) + sizeof( int ) ) ) );
 				getchar();
 
-				//RebuidDatabase( &database, pBuffer );
-
 				( *( int* )pBuffer )++;
+				
+				goto rebuildDatabase;
 				
 				break;
 			
@@ -69,10 +70,11 @@ int main() {
 				}
 				
 				( *( int* )pBuffer )--;
-
-				//RebuidDatabase( &database, pBuffer );
 				
 				getchar();
+				
+				goto rebuildDatabase;
+				
 				break;
 			
 			case '3':
@@ -82,11 +84,11 @@ int main() {
 				break;
 			
 			case '4':
-				void *  tracer = *( void** )database;
-
-				while ( tracer ) {
-					puts( ( char* )tracer );
-					
+				void *  tracer = database;
+				printf( "%p\n", *( void** )tracer );
+				
+				while ( tracer ) {  // Frees the database.
+					printf( "%s\n", *( char** )( ( char* )tracer + sizeof( void* ) * 2 ) );
 					tracer = *( void** )tracer;
 				}
 				
@@ -103,79 +105,76 @@ int main() {
 		}
 	}
 
+	rebuildDatabase:
+	/*
+	#define TRACER ( *( void ** )( ( ( char * )pBuffer + ( *( int* )pBuffer ) * PERSON_SIZE ) + PBUFFER_OFFSET ) )
+	#define PREVIOUS ( *( void ** )( ( ( char * )pBuffer + ( *( int* )pBuffer ) * PERSON_SIZE ) + PBUFFER_OFFSET + sizeof( void* ) ) )
+	#define NEW_NODE ( *( void ** )( ( ( char * )pBuffer + ( *( int* )pBuffer ) * PERSON_SIZE ) + PBUFFER_OFFSET + sizeof( void* ) * 2 ) )
+	#define REBUILD_ITERATOR ( *( int * )( ( ( char * )pBuffer + ( *( int* )pBuffer ) * PERSON_SIZE ) + PBUFFER_OFFSET + sizeof( void* ) * 3 ) )
+	*/
+
+	#define NEXT PREVIOUS
+
+	void *  TRACER;
+	void *  PREVIOUS;
+	void *  NEW_NODE;
+	int     REBUILD_ITERATOR;
+
+	if ( !( pBuffer = realloc( pBuffer, ( ( *( int* )pBuffer ) * PERSON_SIZE ) + PBUFFER_OFFSET + sizeof( void * ) * 2 ) ) ) {  // Allocs spcace for the tracer and previous variables.
+		goto outOfMemory;
+	}
+
+	TRACER = database;
+
+	while ( TRACER != NULL ) {  // Frees the database.
+		NEXT = *( void** )TRACER;
+		free( TRACER );
+		TRACER = NEXT;
+	}
+
+	database = NULL;
+
+	if ( *( int* )pBuffer >= 1 ) {  // First node
+		NEW_NODE = malloc( NODE_SIZE );
+		database = NEW_NODE;
+
+		*( void** )( ( char* )NEW_NODE ) = NULL;  // Next
+		*( void** )( ( char* )NEW_NODE + sizeof( void* ) ) = NULL;  // Previous
+		*( void** )( ( char* )NEW_NODE + sizeof( void* ) * 2 ) = ( void * )( ( char* )pBuffer + PBUFFER_OFFSET );  // Data
+
+		REBUILD_ITERATOR = 1; // REBUILD_ITERATOR is set to 1 as we have the first node already.
+	
+		while ( REBUILD_ITERATOR < *( int* )pBuffer ) {
+			TRACER = database;
+
+			while ( *( void** )TRACER && strcmp( *( char** )TRACER, *( char** )( *( int* )pBuffer + REBUILD_ITERATOR * PERSON_SIZE + PBUFFER_OFFSET ) ) < 1 ) {
+				PREVIOUS = TRACER;
+				TRACER = *( void** )TRACER;
+			}
+
+			NEW_NODE = malloc( NODE_SIZE );
+
+			if ( *( void** )( ( char* )TRACER + sizeof( void * ) ) == NULL ) {  // Sets the database head to the new node if it is the first.
+				database = NEW_NODE;
+			}
+			
+			*( void** )TRACER = NEW_NODE;  // Next from the previous node
+			*( void** )( ( char* )NEW_NODE + sizeof( void* ) ) = PREVIOUS;  // Previous of the new node
+			*( void** )( ( char* )NEW_NODE + sizeof( void* ) * 2 ) = *( char** )TRACER;  // Data of the new node
+
+			REBUILD_ITERATOR++;
+		}
+	}
+
+	if ( !( pBuffer = realloc( pBuffer, ( ( *( int* )pBuffer ) * PERSON_SIZE ) + PBUFFER_OFFSET ) ) ) {  // Deallocs the variables.
+		goto outOfMemory;
+	}
+
+	goto menu;
+
 	outOfMemory:
 	puts( "Fatal: out of memory." );
 	exit( 1 );
 
 	return 0;
-}
-
-void ClearDatabase( void ** database ) {
-/*
-====================
-ClearDatabase
- Clears the
- database.
-====================
-*/
-	
-	void **  tracer = database;
-	void *   old;
-
-	old = *tracer;
-	free( old );
-
-	while ( *tracer ) {
-		old = *tracer;
-		tracer = *tracer;
-		free( old );
-	}
-
-	*database = NULL;
-}
-
-void RebuidDatabase( void ** database, void * pBuffer ) {  // BROKEN
-/*
-====================
-RebuldDatabase
- Rebuilds the
- database from
- pBuffer.
-====================
-*/
-
-	#define CURRENT_STRING ( ( char* )pBuffer + *iterator * PERSON_SIZE + PBUFFER_OFFSET )
-
-	int *    iterator = malloc( sizeof( int ) );
-	void **  tracer;
-	void *   previous;
-	void *   newNode;
-
-	*database = NULL;
-	tracer = database;
-
-	*iterator = 0;
-
-	while( *iterator < *( int* )pBuffer ) {
-		newNode = malloc( NODE_SIZE );
-
-		while ( tracer && strcmp( ( char* )tracer, CURRENT_STRING ) > 1 ) {
-			previous = *tracer;
-			*tracer = **( void*** )tracer;
-		}
-
-		*tracer = newNode;  // Next.
-
-		if ( *iterator == 0 ) {  // Previous.
-			*( void** )( ( char* )tracer + sizeof( void * ) ) = NULL;
-		} else {
-			*( void** )( ( char* )tracer + sizeof( void * ) ) = previous;
-		}
-
-		*( void** )( ( char* )tracer + sizeof( void * ) * 2 ) = CURRENT_STRING;  // Datum.
-	
-		( *iterator )++;
-	}
-
-	free( iterator );
 }
