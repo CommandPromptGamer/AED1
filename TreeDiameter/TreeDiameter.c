1e4042001c3d622c60884d12ca9fb63b414671b9
CommandPromptGamer <46406111+CommandPromptGamer@users.noreply.github.com>
2023-04-15 15:22:46 -0300

    Change indentation to tabs per convention

--- TreeDiameter/TreeDiameter.c
+++ TreeDiameter/TreeDiameter.c
@@ -5,9 +5,9 @@
 #include <time.h>
 
 struct TreeNode {
-    int val;
-    struct TreeNode *left;
-    struct TreeNode *right;
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
 };
 
 int diameterOfBinaryTree(struct TreeNode* root);
@@ -17,53 +17,53 @@ void FillTreeRandomly( struct TreeNode** root );
 int maximum = 0;  // Global maximum variable
 
 int main() {
-    struct TreeNode* root = NULL;
+	struct TreeNode* root = NULL;
 
-    srand( time( NULL ) );
+	srand( time( NULL ) );
 
-    for( int i = 0; i < 100; i++ ) {
-        FillTreeRandomly( &root );
-    }
+	for( int i = 0; i < 100; i++ ) {
+		FillTreeRandomly( &root );
+	}
 
-    printf( "%d\n", diameterOfBinaryTree( root ) );
+	printf( "%d\n", diameterOfBinaryTree( root ) );
 
-    return 0;
+	return 0;
 }
 
 int diameterOfBinaryTree(struct TreeNode* root){
-    FindDown( root );
+	FindDown( root );
 
-    return maximum;
+	return maximum;
 }
 
 int FindDown( struct TreeNode* branch ) {
-    int left;
-    int right;
+	int left;
+	int right;
 
-    if ( branch->left != NULL ) {
-        left = FindDown( branch->left );
-    } else {
-        left = 0;
-    }
+	if ( branch->left != NULL ) {
+		left = FindDown( branch->left );
+	} else {
+		left = 0;
+	}
 
-    if ( branch->right != NULL ) {
-        right = FindDown( branch->right );
-    } else {
-        right = 0;
-    }
+	if ( branch->right != NULL ) {
+		right = FindDown( branch->right );
+	} else {
+		right = 0;
+	}
 
-    maximum = left + right > maximum ? left + right : maximum;
-    return 1 + ( left > right ? left : right );
+	maximum = left + right > maximum ? left + right : maximum;
+	return 1 + ( left > right ? left : right );
 }
 
 void FillTreeRandomly( struct TreeNode** root ) {
-    if ( *root == NULL ) {
-        *root = malloc( sizeof( struct TreeNode ) );
+	if ( *root == NULL ) {
+		*root = malloc( sizeof( struct TreeNode ) );
 
-        ( *root )->val = rand() % 201 - 100;
-        ( *root )->left = NULL;
-        ( *root )->right = NULL;
-    } else {
-        rand() > RAND_MAX / 2 ? FillTreeRandomly( &( *root )->left ) : FillTreeRandomly( &( *root )->right );
-    }
+		( *root )->val = rand() % 201 - 100;
+		( *root )->left = NULL;
+		( *root )->right = NULL;
+	} else {
+		rand() > RAND_MAX / 2 ? FillTreeRandomly( &( *root )->left ) : FillTreeRandomly( &( *root )->right );
+	}
 }
